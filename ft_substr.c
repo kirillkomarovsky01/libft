23a11acfa9a594d1cc26faf7d3b84b0817390f5b
kirillkomarovsky01 <komarovskycyril@yandex.ru>
2021-04-16 23:05:12 +0300

    add substr

---
+++ ft_substr.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t index;
+	char *dest;
+	int lens;
+
+	lens = ft_strlen(s);
+	index = 0;
+	if (!s)
+		return (NULL);
+	if (lens < (int)start)
+		len = 0; 
+	dest = malloc(len + 1);
+	if (!dest)
+		return (NULL);
+	while (s[start + index] && len > 0)
+	{
+		dest[index] = s[start + index];
+		len--;
+		++index;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
