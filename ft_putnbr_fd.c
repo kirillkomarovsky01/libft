7f543cc64ad1502bdceb9ddcf30d8b6a1507d9ae
kirillkomarovsky01 <komarovskycyril@yandex.ru>
2021-04-19 17:28:48 +0300

    add soooom func

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long int num;
+
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = -num;
+	}
+	if (num >= 10)
+	{
+		ft_putnbr_fd(num / 10, fd);
+		ft_putchar_fd(48 + num % 10, fd);
+	}
+	else
+		ft_putchar_fd(48 + num % 10, fd);
+}
