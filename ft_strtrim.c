fa329cfe73e2d3c36c34e792b98a4f6c2c5a6ccc
kirillkomarovsky01 <komarovskycyril@yandex.ru>
2021-04-19 15:37:28 +0300

    add strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,15 @@
+#inlcude "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t len;
+
+	len = ft_strlen(s1);
+	if (!s1)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1)
+		++s1;
+	while (ft_strchr(set, s1[length] && len)
+		len--;
+	return (ft_substr(s1, 0, len + 1));
+}
